201f6c2464f8a5b56ab138d84638f92b6ec0eced
AndreyBodulin <andrey_bodulin@mail.ru>
2021-03-14 14:25:09 +0400

    task: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let answer;
+    if (n >= 0){
+        let str = String(n);
+        let arr = str.split("").reverse().join("");
+        console.log(arr);
+        answer = Number(String(arr));
+    }else {
+        n = n * -1;
+        let str = String(n);
+        let arr = str.split("").reverse().join("");
+        console.log(arr);
+        answer = Number(String(arr));
+    }
+  return answer;
 }
